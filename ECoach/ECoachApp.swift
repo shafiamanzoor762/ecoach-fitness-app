3c18be1fbb99d8b94dd05e9d9a5ffff394ee42e9
shafiamanzoor762 <shafiamanzoor762@gmail.com>
2025-04-13 18:38:15 +0500

    Initial Commit

---
+++ ECoach/ECoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECoachApp.swift
+//  ECoach
+//
+//  Created by apple on 13/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ECoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
